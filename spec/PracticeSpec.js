96b44ba223864197e2b1ab045ef3aeaba6e2d3e3
Anhtai Huynh <anhtaih89@gmail.com>
2016-04-24 15:38:22 -0700

    Add new prompt

--- spec/PracticeSpec.js
+++ spec/PracticeSpec.js
@@ -1,34 +1,90 @@
 
-describe("Practice", function() {
+describe('Practice your logical thinking and "Algorithm of an Algorithm" here.', function() {
 
-  var practice; 
+  describe('Kaprekars Constant', function() {
+    var practice; 
 
-  beforeEach(function() {
-    practice = new Practice();
+    beforeEach(function() {
+      practice = new Kaprekars();
+    });
 
+    it('should work when a single digit number is passed in', function() {
+      expect(practice.constant(9)).toEqual(4);
+    });
+
+    it('should work when a two digit number is passed in', function() {
+      expect(practice.constant(21)).toEqual(3);
+    });
+
+    it('should work when a three digit number is passed in', function() {
+      expect(practice.constant(142)).toEqual(7);
+    });
+
+    it('should work when a four digit number is passed in', function() {
+      expect(practice.constant(2705)).toEqual(6);
+    });
+
+    it('should work when input number begins with zeroes', function() {
+      expect(practice.constant(0125)).toEqual(7);
+      expect(practice.constant(0052)).toEqual(2);
+      expect(practice.constant(0005)).toEqual(6);
+    });           
   });
+  
 
-  it("should work when a single digit number is passed in", function() {
-    expect(practice.kConstant(9)).toEqual(4);
-  });
+  describe('Blackjack Highest', function() {
+    var a = ['king','queen','ace'];
+    var b = ['three', 'nine', 'ace'];
+    var c = ['four', 'two', 'jack'];
+    var d = ['seven', 'five', 'queen', 'three', 'eight'];
+    var e = ['ten', 'six', 'five'];
+    var f = ['jack', 'queen', 'ten'];
+    var g = ['three'];
+    var h = ['ace', 'two', 'three', 'four','five', 'six'];
+    var i = ['ace', 'jack'];
 
-  it("should work when a two digit number is passed in", function() {
-    expect(practice.kConstant(21)).toEqual(3);
-  });
+    var blackjack; 
 
-  it("should work when a three digit number is passed in", function() {
-    expect(practice.kConstant(142)).toEqual(7);
-  });
+    beforeEach(function() {
+      blackjack = new BlackjackHighest();
+    });
 
-  it("should work when a four digit number is passed in", function() {
-    expect(practice.kConstant(2705)).toEqual(6);
-  });
+    it('should output "blackjack king" with input: ' + JSON.stringify(a), function() {
+      expect(blackjack.hitMe(a)).toEqual('blackjack king');
+    });
 
-  it("should work when input number begins with zeroes", function() {
-    expect(practice.kConstant(0125)).toEqual(7);
-    expect(practice.kConstant(0052)).toEqual(2);
-    expect(practice.kConstant(0005)).toEqual(6);
-  });
+    it('should output "below nine" with input: ' + JSON.stringify(b), function() {
+      expect(blackjack.hitMe(b)).toEqual('below nine');
+    });
+
+    it('should output "below jack" with input: ' + JSON.stringify(c), function() {
+      expect(blackjack.hitMe(c)).toEqual('below jack');
+    });
+
+    it('should output "above queen" with input: ' + JSON.stringify(d), function() {
+      expect(blackjack.hitMe(d)).toEqual('above queen');
+    });
+
+    it('should output "blackjack ten" with input: ' + JSON.stringify(e), function() {
+      expect(blackjack.hitMe(e)).toEqual('blackjack ten');
+    }); 
+
+    it('should output "above queen" with input: ' + JSON.stringify(f), function() {
+      expect(blackjack.hitMe(f)).toEqual('above queen');
+    }); 
+
+    it('should output "below three" with input: ' + JSON.stringify(g), function() {
+      expect(blackjack.hitMe(g)).toEqual('below three');
+    }); 
+
+    it('should output "blackjack six" with input: ' + JSON.stringify(h), function() {
+      expect(blackjack.hitMe(h)).toEqual('blackjack six');
+    }); 
+
+    it('should output "blackjack ace" with input: ' + JSON.stringify(i), function() {
+      expect(blackjack.hitMe(i)).toEqual('blackjack ace');
+    }); 
+  }); 
 
 });
 
--- src/practice.js
+++ src/practice.js
@@ -1,35 +1,68 @@
-function Practice() {
-}
 
-Practice.prototype.kConstant = function(num) {
+
+function Kaprekars() {}
+
+Kaprekars.prototype.constant = function(num) {
 
 //Write the algorithm in here!
 
 };
 
+// Context:
+//
+// The function above should be able to take in a num between 1 - 9999 that will return
+// the correct output.
+//
+// Your program should perform the following routine on the number: 
+//
+// 1. Arrange the digits in descending AND ascending order to fit it to a 4-digit number.
+//   a. If num is not a 4-digit number, add zeroes to the end until it is, and then repeat step 1.
+//      Example: 1732 becomes 7321 (desc) and 1237 (asc) || 21 becomes 2100 and 0012, etc.
+//
+// 2. Get the difference of the bigger and smaller number. Repeat step 1 with the difference.
+//
+// The function should return the number of times this routine must be performed before reaching 6174,
+// aka 'Kaprekars Constant'. The reason is 6174 will always result in 6174 with this routine (7641 - 1467 = 6174). 
+//
+// For example: if num is 3524, your program should return 3 because of the following steps: 
+//
+// 1st call: 5432 - 2345 = 3087
+// 2nd call: 8730 - 0378 = 8352 
+// 3rd call: 8532 - 2358 = 6174
 
 
-/*
-Context:
+/* --------------------------------------- */
 
-The function above should be able to take in a num between 1 - 9999 that will return
-the correct output.
+function BlackjackHighest() {}
 
-Your program should perform the following routine on the number: 
+BlackjackHighest.prototype.hitMe = function(array) {
+  
+//Write the algorithm in here!
 
-1. Arrange the digits in descending AND ascending order to fit it to a 4-digit number.
-  a. If num is not a 4-digit number, add zeroes to the end until it is, and then repeat step 1.
-     Example: 1732 becomes 7321 (desc) and 1237 (asc) || 21 becomes 2100 and 0012, etc.
+};
 
-2. Get the difference of the bigger and smaller number. Repeat step 1 with the difference.
-
-The function should return the number of times this routine must be performed before reaching 6174,
-aka 'Kaprekars Constant'. The reason is 6174 will always result in 6174 with this routine (7641 - 1467 = 6174). 
-
-For example: if num is 3524, your program should return 3 because of the following steps: 
-
-1st call: 5432 - 2345 = 3087
-2nd call: 8730 - 0378 = 8352 
-3rd call: 8532 - 2358 = 6174
-
-*/
+// Context: 
+//
+// Create the hitMe function to take in an array of numbers and letters
+// representing blackjack cards which will be written out (eg ['two', 'three']). 
+// The array can have any number of cards.
+//
+// The function should output "below", "above", or "blackjack", signifying
+// that all numbers add up to 21, along with the highest card in that set. 
+// IE: ["ten, "five", "six"] should output "blackjack ten", because ten is
+// the highest in the set. 
+//
+// If lower than 21, then the output should be below 21, along with the highest
+// card in that set. IE: ["eight", "six"] should output "below eight".
+//
+// If higher than 21, then the output should be above 21, along with the highest
+// card in that set. IE: ["three", "four", "queen", "nine"] should output
+// "above queen".
+//
+// If you have multiple face cards, the order from of importance is jack, queen,
+// and king, with king being the highest (even though their value in blackjack is 10).
+//
+// You must account for the ace as well, which in blackjack can be either 1
+// or 11. Your ace cannot be 11 if it puts you over 21. If it puts you at exactly
+// 21, such as with input ['ace', 'five', 'six'], your output should be 
+// "blackjack ace", since ace is your highest card.
